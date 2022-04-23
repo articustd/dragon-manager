5667235a36bba62b495ebf1389a10abd9097db83
Articus <articusthedata@gmail.com>
2022-04-23 10:09:14 -0700

    Init push of framework

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+import storyConfig from './config.json'
+Config = { ...Config, ...storyConfig };
+setup.ImagePath = "assets/";
+
+((Config, State, Story, Engine, Dialog, $document) => {
+	$(document.head).append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous">')
+	// Set State Variables
+
+	// Register custom SugarCube macros
+	// registerAlert(Macro, Dialog);
+
+	// Register plugin modules
+	// registerTitleMatchProperties({ State, Story, $document });
+
+	// Register components
+	// registerHeader($document);
+
+	// Setup noreturn
+	// $document.on(':passagestart', function (ev) {
+	// 	if (!ev.passage.tags.includes('noreturn'))
+	// 		State.variables.return = ev.passage.title;
+	// });
+})(Config, State, Story, Engine, Dialog, $(document));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    mode: 'production',
+    entry: './src/js/index.js',
+    output: {
+        filename: 'story.bundle.js'
+    },
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components|story|vendor)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            "@util": path.resolve(__dirname, 'src', 'js', 'controller','util'),
+            "@controller": path.resolve(__dirname, 'src', 'js', 'controller'),
+            "@js": path.resolve(__dirname, 'src', 'js')
+        },
+        extensions: ['.js', '.json']
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
+    },
+};
