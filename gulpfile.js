098736f675e65c0352af23b8d090f7afdf7cc45b
Articus <articusthedata@gmail.com>
2022-04-29 07:02:05 -0700

    Updated gulp to have maxStates

--- gulpfile.js
+++ gulpfile.js
@@ -36,12 +36,12 @@ task(function buildTwee() {
 
 // Configure Environments
 task(function configDev() {
-    let config = '{"history": {"controls": true }, "debug": true, "logging": true }'
+    let config = '{"history": {"controls": true, "maxStates": 2  }, "debug": true, "logging": true }'
     return writeConfig(config)
 })
 
 task(function configProd() {
-    let config = '{"history": {"controls": true }, "debug": true, "logging": true }'
+    let config = '{"history": {"controls": true, "maxStates": 2  }, "debug": true, "logging": true }'
     return writeConfig(config)
 })
 
