c2287cccecc5b8d9b7fb67aa7d60c39d3c7ff83b
Articus <articusthedata@gmail.com>
2022-04-29 17:35:39 -0700

    Removed a logger

--- src/js/macro/bars/golemPopBar.js
+++ src/js/macro/bars/golemPopBar.js
@@ -11,7 +11,7 @@ Macro.add('golemPopBar', {
         let $barText = $('<span/>').css({'z-index':'1', 'mix-blend-mode':'difference', 'padding': '5px'})
         let $bar = $('<div/>').css({ 'width': getWidth(golem.currSpawnRate, golem.spawnRate), 'height': '100%', 'background-color': 'white', 'position': 'absolute', 'top': '0px', 'left': '0px', 'z-index': '-1' })
 
-        golem.on('popTick', function ({ currSpawnRate, spawnRate }) { logger('here');$bar.css({ 'width': getWidth(currSpawnRate, spawnRate) }) })
+        golem.on('popTick', function ({ currSpawnRate, spawnRate }) { $bar.css({ 'width': getWidth(currSpawnRate, spawnRate) }) })
         
         if(this.payload[0])
             $barText.wiki(this.payload[0].contents)
