516e2446cafb02604067299427a518ecd90e06b7
Articus <articusthedata@gmail.com>
2022-05-23 07:59:21 -0700

    Fixed click issue with player action

--- src/js/macro/actions/gatherResourceAction.js
+++ src/js/macro/actions/gatherResourceAction.js
@@ -5,21 +5,21 @@ Macro.add('gatherResourceAction', {
     handler: function () {
         let [actionName] = this.args
         let action = getScene('StartGame').getAction(actionName)
-        let $barContainer = $('<div/>').css({'width': '200px', 'height': '30px', 'position': 'relative', 'cursor':'pointer'})
+        let $barContainer = $('<div/>').css({'width': '200px', 'height': '30px', 'position': 'relative'})
         let $buttonText = $('<span/>').css({'z-index':'1', 'mix-blend-mode':'difference', 'padding': '5px'}).wiki(actionName)
         let $bar = $('<div/>').css({ 'width': getWidth(action.cooldown, action.baseCooldown), 'height': '100%', 'background-color': 'white', 'position': 'absolute', 'top': '0px', 'left': '0px', 'z-index': '-1' })
 
         action.on(`${actionName}CooldownChange`, function({cooldown, baseCooldown}) {
             $bar.css({ 'width': getWidth(cooldown, baseCooldown) })
             if (cooldown === baseCooldown) {
-                $barContainer.css({'cursor':'pointer'})
                 barClick($barContainer, action)
             }
         })
 
         $barContainer.append($buttonText).append($bar)
 
-        barClick($barContainer, action)
+        if(action.cooldown === action.baseCooldown)
+            barClick($barContainer, action)
 
         $(this.output).append($barContainer)
     }
@@ -30,6 +30,7 @@ function getWidth(curr, max) {
 }
 
 function barClick($barContainer, action) {
+    $barContainer.css({'cursor':'pointer'})
     $barContainer.click(()=>{
         action.gatherResource()
         $barContainer.css({'cursor':'default'})
