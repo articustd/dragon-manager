1f3ac570da5b0349088b08a340c9d9ff2abb407e
Articus <articusthedata@gmail.com>
2022-05-24 11:00:30 -0700

    Reduced emitter lisenters

--- src/js/macro/UI/hideUntilBuilt.js
+++ src/js/macro/UI/hideUntilBuilt.js
@@ -9,10 +9,11 @@ Macro.add('hideUntilBuilt', {
         let building = getScene('StartGame').getBuilding(buildingName)
         let $wrapper = $('<div/>').css('display', (building.purchased) ? 'block' : 'none').wiki(this.payload[0].contents)
 
-        building.on('PurchasedChange', (purchased) => {
-            if (purchased)
-                $wrapper.css('display', 'block')
-        })
+        if (!building.purchased)
+            building.on('PurchasedChange', (purchased) => {
+                if (purchased)
+                    $wrapper.css('display', 'block')
+            })
 
         $(this.output).append($wrapper)
     }
